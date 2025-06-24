216bb09438b92b5d4388dfd55f5c4c196c282067
Dheerendra Shivhare <138848710+Dheerendrashivhare@users.noreply.github.com>
2025-06-24 06:32:50 +0000

    Created the Project 8 : Calculator

--- demo_code.py
+++ demo_code.py
@@ -1,11 +1,29 @@
 
-def first_non_repating_char(string):
-    for j in range(len(string)):
-        string2 = string
-        string2 = string2[:j] + string2[j+1:]
-        if string[j] not in string2 :
-            return string2[j]
+class Prectice_questions:
 
-    return None
+    @staticmethod
+    def first_non_repating_char(string):
+        for j in range(len(string)):
+            string2 = string
+            string2 = string2[:j] + string2[j+1:]
+            if string[j] not in string2 :
+                return string2[j]
+    
+        return None
 
-print(first_non_repating_char("abababab"))
+    @staticmethod
+    def find_samllest_second_non_existing_int(data_list:list):
+        missing_numbers = []
+        smallest_positive_int = 1
+        for i in data_list :
+            pass
+                        
+    
+        
+print("Start")
+print(Prectice_questions.first_non_repating_char("abababab"))
+
+    
+
+
+
